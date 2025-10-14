ff60f3686115c6a8d2ea7385a444cb0100f8f779
HBFL3Xx <105460420+HBFLEX@users.noreply.github.com>
2025-10-14 02:42:00 +0200

    prepare for npm publish

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-export * from './core/FirestoreRepository';
+export { FirestoreRepository, ID, HookEvent } from './core/FirestoreRepository';
+export { FirestoreQueryBuilder } from './core/QueryBuilder';
+
+export {
+    NotFoundError,
+    ValidationError,
+    ConflictError,
+    FirestoreIndexError
+} from './core/Errors';
+
+export { parseFirestoreError } from './core/ErrorParser';
+export { errorHandler } from './core/ErrorHandler';
+
+export { makeValidator, Validator } from './core/Validation';
